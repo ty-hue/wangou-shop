64d22bd82c15abc635a6ac6942472b9b54603e1b
TangYang <1553604051@qq.com>
2026-07-27 08:12:43 +0800

    fix：改用@prisma/adapter-neon适配器(websocket)替换原本的@prisma/adapter-pg适配器(tcp)，更快的连接云数据库

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,13 @@
 import "server-only";
 
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "./generated/prisma/client";
 
 // 创建数据库连接实例
 export async function createPrismaClient() {
-  const pool = new Pool({
+  const adapter = new PrismaNeon({
     connectionString: process.env.DATABASE_URL,
   });
-  const adapter = new PrismaPg(pool);
   const prisma = new PrismaClient({ adapter });
   return prisma;
 }
